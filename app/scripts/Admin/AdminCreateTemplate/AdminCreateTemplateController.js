aa4ed543e6015aa57856bbdfc42885a851ef45d6
hafsteinn <hafsteinn11@ru.is>
2014-03-15 14:12:24 +0000

    working on controller

--- app/scripts/Admin/AdminCreateTemplate/AdminCreateTemplateController.js
+++ app/scripts/Admin/AdminCreateTemplate/AdminCreateTemplateController.js
@@ -4,58 +4,89 @@ angular.module('EduRateApp').controller('AdminCreateTemplateController',
 ['$rootScope','$scope', 'adminFactory', '$location',
 function($rootScope, $scope, adminFactory, $location){
 
-	var numberOfexQ = 0;
-	$scope.numberOfexQuistions = [1,2,3,4];
 	$scope.userObject = $rootScope.userObject;
-	//change the number of X Questions
-	$("#numberOfexQuestion").click(function(e) {
-		numberOfexQ = $("#numberOfexQuestion :selected").val();
-		console.log(numberOfexQ);	
-
-		//set the number of X questions as selected
-		for (var i = 1; i <= numberOfexQ; i++) {
-			$scope.numberOfexQuistions.push(i);
-		};
-		console.log($scope.numberOfexQuistions);
-	}); 
-
-
-	
 	$(':radio').radio();
 
-	$scope.Answers = {
+	$scope.evaluationTemplate = {
+		TitleIS: $scope.TitleIS,
+		TitleEN: $scope.TitleEN,
+		IntroTextIS: $scope.IntroTextIS,
+		IntroTextEN: $scope.IntroTextEN,
+		CourseQuestions: [],
+		TeacherQuestions: []
+	};
+
+	function newTextCourseQuestion(iTextIS,iTextEN,iImageURL){
+		var newTextCourseQuestion = {
+			TextIS: iTextIS,
+			TextEN: iTextEN,
+			ImageURL : iImageURL,
+			Type : 'text',
+			Answers : []
+		};
+
+		return newTextCourseQuestion;
+	}
+
+	function newTextTeacherQuestion(iTextIS,iTextEN,iImageURL){
+		var newTextTeacherQuestion = {
+			TextIS: iTextIS,
+			TextEN: iTextEN,
+			ImageURL : iImageURL,
+			Type : 'text',
+			Answers : []
+		};
+
+		return newTextTeacherQuestion;
+	}
+
+
+	//fall sem er keyrt þegar user gerir save
+	$scope.saveText = function(){
+		//athuga hvort sé hakað í course eða teacher
+		if($('#courseCheck').prop('checked'))
+		{
+			var newT = newTextCourseQuestion(
+							$scope.CTextIS,
+							$scope.CTextEN,
+							$scope.CimageURL);
+
+			$scope.evaluationTemplate.CourseQuestions.push(newT);
+			console.log('the template so far :' + $scope.evaluationTemplate);
+		}
+		else if($('#teacherCheck').prop('checked'))
+		{
+			var newT = newTextTeacherQuestion(
+							$scope.CTextIS,
+							$scope.CTextEN,
+							$scope.CimageURL);
+
+			$scope.evaluationTemplate.TeacherQuestions.push(newT);
+		}
+		else
+		{
+			alert('Þú verður að velja annaðhvort námskeið eða kennara');
+		}
+	}
+
+
+	$scope.CourseAnswers = {
 		TextIS: '',
 		TextEN: '',
 		ImageURL: '',
 		Weight: '',
 	};
 
-	$scope.CourseQuestions = {
+	$scope.TeacherAnswers = {
 		TextIS: '',
 		TextEN: '',
 		ImageURL: '',
-		Type: '',
-		Answers: []
+		Weight: '',
 	};
 
-	$scope.evaluationTemplate = {
-		TitleIS: '',
-		TitleEN: '',
-		IntroTextIS: '',
-		IntroTextEN: '',
-		CourseQuestions: $scope.CourseQuestions,
-		TeacherQuestions: []
-	}
 
-	$scope.showCourseTextQuestion = function(){
-		$('.textQuestion').show();
-	};
-
-	$scope.hideCourseTextQuestion = function(){
-		$('.textQuestion').hide();
-	};
-
-	//this function creates a new evaluation template
+	//þegar ýtt er á save á að keyra þetta (s.s.notandi vill save-a allt templateið)
+	//þá þarf líklega líka að núllstilla evaluationTemplate objectið
 	//adminFactory.newEvaluationTemplate($scope.evaluationTemplate);
 
 }]);
